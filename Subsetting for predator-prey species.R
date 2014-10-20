90da98631a426a815b80f45f4b7afc3cff0c125c
Michael <auriemma@Bio-HurlbertL01-V220.ad.unc.edu>
2014-10-20 15:46:51 -0400

    script for subset indiv prey spp

--- Subsetting for predator-prey species.R
+++ Subsetting for predator-prey species.R
@@ -3,12 +3,7 @@ predatorspecies = c('Predator1', 'Predator2', 'Predator3')
 
 #Making subset of data for a single predator species
 yos.subset = subset (yos, Predator.Species.Name %in% predatorspecies)
+atlsharpnose = subset (groundsharks, source_taxon_name %in% predsp)
 
-preylist = yos[Predator.Species.Name %in% predatorspecies, 'Prey.Species.Name']
-
-names(yos)
-[1] "Latitude"              "Longitude"             "Year"                 
-[4] "ICES.StomachID"        "Predator"              "Predator..mean..Lengh"
-[7] "Prey.Species.Name"    
-
+#Getting prey list of prey data for indiv species
 preylist = yos[yos$Predator %in% predatorspecies, 'Prey.Species.Name']
