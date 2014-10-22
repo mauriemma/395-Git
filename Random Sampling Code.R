0b36ce442034edf23b2f235ca222cd8f7d0d53a1
Michael Auriemma <auriemma@live.unc.edu>
2014-10-22 16:18:09 -0400

    saved environ

---
+++ Random Sampling Code.R
@@ -0,0 +1,34 @@
+preyitems = data.frame(table(yos$Prey.Species.Name))
+preyitems2 = preyitems[preyitems$Var1 != "", ]
+preyitems2 = preyitems2[order(preyitems2$Freq, decreasing = T), ]
+
+
+
+numitems = c(10,50)
+
+numsamples = 1000
+
+output = c()
+
+for (i in numitems) {
+  for (j in 1:numsamples) {
+    dietsamp = sample(preyitems2$Var1, i, prob = preyitems2$Freq, replace = T)
+    samp.num.items = length(unique(dietsamp))
+    output = rbind(output, c(i, samp.num.items))
+  }
+}
+out = data.frame(output)
+names(out) = c('ItemSampleSize', 'NumberPreyTypes')
+
+#Calculate mean and standard deviation for the expected number of diet types
+#for a given sample size
+
+out.mean = aggregate(out$NumberPreyTypes, by = list(out$ItemSampleSize), mean)
+out.var = aggregate(out$NumberPreyTypes, by = list(out$ItemSampleSize), var)
+out.summary = cbind(out.mean, out.var$x^0.5)
+names(out.summary) = c('ItemSampleSize', 'MeanNumPreyTypes', 'SDNumPreyTypes')
+
+#Merging tables based on Number Prey Item 
+merge(table x, table y, by.x = column wanted, by.y = corresponding column, all.x=T, all.Y=T)
+
+
