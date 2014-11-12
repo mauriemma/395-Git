b7b07f641bf3a91aa4f3f2b0334af5a3e382b2d5
Michael <auriemma@Bio-HurlbertL01-V220.ad.unc.edu>
2014-11-12 16:17:26 -0500

    added script for prey occurence and saved env and history

--- General R Work.R
+++ General R Work.R
@@ -303,4 +303,15 @@ high.all.tab = high.all.tab[order(high.all.tab$Var1, decreasing = F),]
 #Number prey items for each species in YOS data
 yos.spectotprey= data.frame(table(yos$Predator))
 
+#Percent occurence of each prey type in a species
+bonnethead.preyfreq = aggregate(bonnethead$tmp_and_unique_source_specimen_id, by = list(bonnethead$target_taxon_name), 
+                                function(x) length(unique(x)))
+tiger.preyfreq = aggregate(tigershark$tmp_and_unique_source_specimen_id, by = list(tigershark$target_taxon_name), 
+                                function(x) length(unique(x)))
+dusky.preyfreq = aggregate(dusky$tmp_and_unique_source_specimen_id, by = list(dusky$target_taxon_name), 
+                           function(x) length(unique(x)))
+whitetip.preyfreq = aggregate(whitetip$tmp_and_unique_source_specimen_id, by = list(whitetip$target_taxon_name),
+                              function(x) length(unique(x)))
+
+names(-----) = c('PreySpecies','PercentOccurence')
 
