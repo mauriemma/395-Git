7126412005f63f51502c46f32d799f90ef89b231
Michael <auriemma@Bio-HurlbertL01-V220.ad.unc.edu>
2014-11-20 15:01:24 -0500

    moved all plotting script to plot file

--- data plotting script.R
+++ data plotting script.R
@@ -24,7 +24,34 @@ points(bull.summary$NumberStomachs, bull.summary$MeanNumPreyTypes, type = "l", c
 
 plot(bonnethead.new$NumberStomachs, bonnethead.new$NumberPreyTypes, pch = 16, col = 'gray50', xlim = c(2, 22), ylim=c(1,15), xlab = "Number of Stomachs", ylab = "", main = 'S. tiburo')
 points(bonnethead.summary$NumberStomachs, bonnethead.summary$MeanNumPreyTypes, type = "l", col= "red", lwd= 5)
+par(mfrow = c(1,1), mar = c(5,5,3,2))
+
+#All curves in one plot
+
+plot(blacktip.summary$NumberStomachs, blacktip.summary$MeanNumPreyTypes, xlim = c(0,110), ylim = c(0,30), xlab = "Number of Stomachs",ylab = "Number of Prey Types", main = "Cumulative Mean Prey Curves", pch = 1, col = "Red", type = "o")
+points(bonnethead.summary$NumberStomachs,bonnethead.summary$MeanNumPreyTypes, pch = 1, col = "Blue", type="o")
+points(bull.summary$NumberStomachs, bull.summary$MeanNumPreyTypes, col = "Green",type="o")
+points(finetooth.summary$NumberStomachs, finetooth.summary$MeanNumPreyTypes, col = "Orange",type="o")
+legend(80,9, legend = "Blacktip", pch=1, col="red", bty = "n")
+legend(80,7, legend = "Bonnethead", pch=1, col="blue",bty = "n")
+legend(80,5, legend = "Bull", pch=1, col = "green",bty = "n")
+legend(80,3, legend = "Finetooth", pch=1, col = "orange", bty = "n")
+
+#Equal sample size curve plots (13 random stomachs)
+plot(bull.summary$NumberStomachs, bull.out.mean$MeanNumPreyTypes, xlim = c(0,15), ylim = c(0,10),
+     xlab = "Number of Stomachs",ylab = "Number of Prey Types", main = "Equal Sample-Size Randomized Prey Curves", 
+     pch =16 , type ="o")
+points(blacktip.summary$NumberStomachs, blacktip.summary$MeanNumPreyTypes, pch=15, type = "o")
+points(bonnethead.summary$NumberStomachs, bonnethead.summary$MeanNumPreyTypes, pch=17, type="o")
+points(finetooth.summary$NumberStomachs, finetooth.summary$MeanNumPreyTypes, pch=18, type="o")
+legend(0,10, legend = "Blacktip", pch=15, bty="n")
+legend(0,9, legend = "Bonnethead", pch=17, bty="n")
+legend(0,8, legend = "Bull", pch=16, bty="n")
+legend(0,7, legend = "Finetooth", pch=18, bty="n")
 
 
-#All curves in one
+#Error bars
+arrows(bull.out.mean$Group.1, bull.out.mean$x, bull.out.mean$Group.1, bull.out.mean$x + bull.out.summary$SDNumPreyTypes, length = 0.1, angle = 90)
+arrows(bull.out.mean$Group.1, bull.out.mean$x, bull.out.mean$Group.1, bull.out.mean$x - bull.out.summary$SDNumPreyTypes, length = 0.1, angle = 90)
+arrows(bonnethead.out.mean$Group.)
 
